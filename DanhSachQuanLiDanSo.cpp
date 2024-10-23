0f37afa784ccdb23c084cd04436c918843ecf7eb
QuangVinh0808 <qvinh023@gmail.com>
2024-10-23 20:39:34 +0700

    commit
    commit

--- DanhSachQuanLiDanSo.cpp
+++ DanhSachQuanLiDanSo.cpp
@@ -7,15 +7,31 @@ struct danso{
     int NumberOfPerson;
 };
 
-// vector<int> YearWithMinPerson(vector<danso> a)
-// {
+vector<int> YearWithMinPerson(vector<danso> a)
+{
+    int min_val = 1e9;
+    for(auto x : a){
+        if(min_val > x.NumberOfPerson) min_val = x.NumberOfPerson;
+    }
+    vector<int> tmp;
+    for(auto x : a){
+        if(x.NumberOfPerson == min_val) tmp.push_back(x.year);
+    }
+    return tmp;
+}
 
-// }
-
-// vector<int> YearWithMaxPerson(vector<danso> a)
-// {
-
-// }
+vector<int> YearWithMaxPerson(vector<danso> a)
+{
+    int max_val = -1;
+    for(auto x : a){
+        if(max_val < x.NumberOfPerson) max_val = x.NumberOfPerson;
+    }
+    vector<int> tmp;
+    for(auto x : a){
+        if(x.NumberOfPerson == max_val) tmp.push_back(x.year);
+    }
+    return tmp;
+}
 
 vector<int> YearWithZeroPerson(vector<danso> a)
 {
@@ -26,18 +42,49 @@ vector<int> YearWithZeroPerson(vector<danso> a)
     return tmp;
 }
 
+vector<int> YearWithSamePerson(vector<danso> a, int n)
+{
+    vector<int> tmp;
+    for(auto x : a)
+    {
+        if(x.NumberOfPerson == n) tmp.push_back(x.year);
+    }
+    return tmp;
+}
+
 int main()
 {
     vector<danso> a(51);
     for(int i = 0; i <= 50; i++){
         cin >> a[i].year >> a[i].NumberOfPerson;
     }
+    
 
     vector<int> listYearZeorPerson = YearWithZeroPerson(a);
     if(listYearZeorPerson.size() == 0) cout << "Khong co nam nao khong co nguoi sinh !";
     else{
-        for(auto it : listYearZeorPerson) cout << "Nhung nam khong co nguoi sinh ra la : " << it << ' ';
+        cout << "Nhung nam khong co nguoi sinh ra la : ";
+        for(auto it : listYearZeorPerson)  cout << it << ' ';
     }
     cout << '\n';
 
+    vector<int> listYearMinPerson = YearWithMinPerson(a);
+    vector<int> listYearMaxPerson = YearWithMaxPerson(a);
+
+    cout << "Nam co nhieu nguoi duoc sinh ra nhat la : ";
+    for(auto x : listYearMaxPerson) cout << x << ' ';
+    cout << '\n';
+
+    cout << "Nam co it nguoi sinh ra nhat la : ";
+    for(auto x : listYearMinPerson) cout << x << ' ';
+    cout << '\n';
+
+    int numberPerson;
+    cout << "Nhung nam co "; cin >> numberPerson; cout << "la\n";
+    vector <int> listYearSamePerson = YearWithSamePerson(a, numberPerson);
+    if(listYearSamePerson.size() != 0)
+    {
+        for(int x : listYearSamePerson) cout << x << ' ';
+    }
+    else cout << "NONE";
 }
